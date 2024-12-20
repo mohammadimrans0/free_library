765ce6dfdbc84fa712c1e97902aacad3e8d4b6b0
Mohoammad Imran <mohoammadimran@Mohammads-MacBook.local>
2024-12-20 21:57:53 +0600

    add guicon

--- library_management_system/asgi.py
+++ library_management_system/asgi.py
@@ -1,11 +1,4 @@
-"""
-ASGI config for library_management_system project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
 
 import os
 
